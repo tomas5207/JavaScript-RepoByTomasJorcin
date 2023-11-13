0c9038366bbb01fccea360b2ff1550aa81bff105
tomas0527 <tomasjorcin@gmail.com>
2023-11-13 15:44:34 -0300

    Evidencia de que intente meter algo con fecth pero al final por ciertas politicas no pude :/

--- clave.js
+++ clave.js
@@ -1,18 +1,24 @@
-var contenido = document.getElementById('contenido');
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
 
 
 document.getElementById('clave').addEventListener('click', function(){
     var insertarClave = document.getElementById('inserte').value;
-    fetch('https://github.com/tomas5207/JavaScript-RepoByTomasJorcin/blob/master/clave.json')
-    .then(response => response.json())
-    .then(data => {
-        if(data.clave == insertarClave){
-            window.location.href= 'index.html';
-        }else{
-            alert("Clave incorrecta...")
+    fetch('clave.json')
+    .then((res) => res.json())
+    .then((data) => {
+        if (data && data.clave == insertarClave) {
+            window.location.href = 'index.html';
+        } else {
+            alert("Clave incorrecta...");
         }
-    }).catch(error => console.error('Error:' , error));
+    }).catch(error => console.error('Error:', error));
 });
+
 
 
 
